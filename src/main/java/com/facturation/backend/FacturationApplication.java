07ae15f7730a1f8be0d9f97105b693c95fa4d13f
Ary Fenitra <87113467+rafenitra@users.noreply.github.com>
2025-06-09 13:57:27 +0200

    Initialisation du projet de facturation

---
+++ src/main/java/com/facturation/backend/FacturationApplication.java
@@ -0,0 +1,13 @@
+package com.facturation.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facturation/backend/FacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturation.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
